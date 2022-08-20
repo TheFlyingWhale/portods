4f667d86090fc9397c4a037265446819e098cb39
Ole Walberg <greblavelo@gmail.com>
2022-08-20 17:48:25 +0200

    add tailwind build script and extend button style

--- src/App.tsx
+++ src/App.tsx
@@ -12,37 +12,14 @@ import {
 
 const App = () => {
     return (
-        <>
-            <Heading>This is h1</Heading>
-            <Heading as="h2">This is h2</Heading>
-            <Heading as="h3" className="text-red-500 text-9xl">
-                This is h3
-            </Heading>
-            <Heading as="h4" className="text-orange-500 text-9xl">
-                This is h4
-            </Heading>
-            <Heading as="h5">This is h5</Heading>
-            <Heading as="h6" className="text-9xl">
-                This is h6
-            </Heading>
-            <Button>This is a button</Button>
-            <Input name="email" placeholder="some placeholder text" />
-            <Label htmlFor="email">Email</Label>
-            <Label htmlFor="rememberMe">Remember me</Label>
-            <Checkbox name="rememberMe" />
-            <VStack>
-                <HStack className="text-blue-500 gap-3">
-                    <Text>1</Text>
-                    <Text>2</Text>
-                    <Text>3</Text>
-                </HStack>
-                <VStack className="text-orange-500 gap-10">
-                    <Text>1</Text>
-                    <Text>2</Text>
-                    <Text>3</Text>
-                </VStack>
+        <VStack className="bg-slate-200 h-screen justify-center items-center">
+            <VStack className="w-1/2 h-1/2 bg-white justify-center items-center rounded-3xl drop-shadow-sm">
+                <Heading className="text-9xl font-bold text-slate-800">
+                    Portods
+                </Heading>
+                <Button className="bg-green-500">This is a button</Button>
             </VStack>
-        </>
+        </VStack>
     );
 };
 
--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
     children: React.ReactNode;
+    className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
-    return <button>{children}</button>;
+const Button: React.FC<ButtonProps> = ({ children, className }) => {
+    return (
+        <button
+            className={twMerge(
+                `bg-blue-500 hover:bg-orange-500 px-6 py-3 rounded-md ${className}`
+            )}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
