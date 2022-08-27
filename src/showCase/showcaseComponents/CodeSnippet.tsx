ed01aec74eba0fb20835d89c24bc2220087d18cf
Ole Walberg <greblavelo@gmail.com>
2022-08-27 13:41:26 +0200

    improve contrast

--- src/showCase/showcaseComponents/CodeSnippet.tsx
+++ src/showCase/showcaseComponents/CodeSnippet.tsx
@@ -6,7 +6,7 @@ interface CodeSnippetProps {
 
 const CodeSnippet: React.FC<CodeSnippetProps> = ({ children }) => {
 	return (
-		<span className="px-2 py-0.5 rounded-md text-sm font-mono text-orange-400 bg-blue-900">
+		<span className="px-2 py-0.5 rounded-md text-sm font-mono text-orange-300 bg-blue-900">
 			<span className="text-neutral-100">{"<"}</span>
 			{children}
 			<span className="text-neutral-100">{" />"}</span>
